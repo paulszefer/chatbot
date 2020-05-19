b9d7e0c3004709ef525b19733e10b87cd4ed30fb
Paul Szefer <pszefer@hotmail.com>
2020-05-18 19:21:44 -0700

    Add chat container

--- public/js/script.js
+++ public/js/script.js
@@ -6,19 +6,25 @@ const socket = io();
 
 const speakButton = document.querySelector('.speakButton');
 const inputBox = document.querySelector('.inputBox');
-const clientText = document.querySelector('.clientText');
-const serverText = document.querySelector('.serverText');
 
 // Framework for voice recognition
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
+recognition.isListening = false;
 recognition.lang = 'en-US';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
 speakButton.addEventListener('click', () => {
-    console.log('Started listening.');
-    recognition.start();
+    if (!recognition.isListening) {
+        console.log('Started listening.');
+        recognition.isListening = true;
+        recognition.start();
+    } else {
+        console.log('Stopped listening.');
+        recognition.isListening = false;
+        recognition.abort();
+    }
 });
 
 recognition.addEventListener('speechstart', () => {
@@ -27,22 +33,26 @@ recognition.addEventListener('speechstart', () => {
 
 recognition.addEventListener('result', (e) => {
     console.log('Speech result has been detected.');
+    recognition.isListening = false;
 
     const last = e.results.length - 1;
     const text = e.results[last][0].transcript;
 
+    console.log('User said: ' + text);
     console.log('Confidence: ' + e.results[0][0].confidence);
 
     handleInput(text);
 });
 
 recognition.addEventListener('speechend', () => {
-    console.log('Stopped listening.');
+    console.log('End of speech has been detected.');
+    recognition.isListening = false;
     recognition.stop();
 });
 
 recognition.addEventListener('error', (e) => {
-    serverText.textContent = 'Error: ' + e.error;
+    console.error('Error: ' + e.error);
+    recognition.isListening = false;
 });
 
 inputBox.addEventListener('keydown', (event) => {
@@ -51,12 +61,14 @@ inputBox.addEventListener('keydown', (event) => {
 
         const text = inputBox.value;
 
+        console.log('User said: ' + text);
+
         handleInput(text);
     }
 });
 
 function handleInput(text) {
-    clientText.textContent = text;
+    addLocalMessage(text);
     socket.emit('chat message', text);
 }
 
@@ -71,11 +83,60 @@ socket.on('bot reply', function (replyText) {
     synthVoice(replyText);
 
     if (replyText == '') replyText = '(No answer...)';
-    serverText.textContent = replyText;
+    addRemoteMessage(replyText);
 });
 
 socket.on('bot error', function (errorMessage) {
-    console.log(errorMessage);
-
-    serverText.textContent = errorMessage;
+    console.error(errorMessage);
+    addRemoteMessage(errorMessage);
 });
+
+////
+
+function addRemoteMessage(text) {
+    const chatContainer = document.getElementsByClassName('chat-container')[0];
+    const newMessageRow = generateRemoteMessageRow(text);
+    chatContainer.appendChild(newMessageRow);
+}
+
+function addLocalMessage(text) {
+    const chatContainer = document.getElementsByClassName('chat-container')[0];
+    const newMessageRow = generateLocalMessageRow(text);
+    chatContainer.appendChild(newMessageRow);
+}
+
+function generateRemoteMessageRow(text) {
+    const row = generateRow();
+    const message = generateMessage(text);
+    message.classList.add('chat-bubble-left');
+    row.appendChild(message);
+    return row;
+}
+
+function generateLocalMessageRow(text) {
+    const row = generateRow();
+    const message = generateMessage(text);
+    message.classList.add('chat-bubble-right');
+    row.appendChild(message);
+    return row;
+}
+
+function generateRow() {
+    const row = document.createElement('div');
+    row.classList.add('chat-row');
+    return row;
+}
+
+function generateMessage(text) {
+    const message = document.createElement('div');
+    message.classList.add('chat-bubble');
+    message.textContent = text;
+    return message;
+}
+
+addRemoteMessage('hey');
+addRemoteMessage('how\'s it going');
+addLocalMessage('not bad');
+addLocalMessage('you?');
+addRemoteMessage('pretty good');
+addLocalMessage('that\'s good');
